ad3ce7265725f209ab310931f52c7f9ae5aba255
frencht0842 <69589659+frencht0842@users.noreply.github.com>
2024-12-06 22:46:17 -0500

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,34 +1,34 @@
 export const products = [
   {
     id: 1,
-    name: "Wireless Headphones",
-    description: "High-quality wireless headphones with noise cancellation",
+    name: "Chestnut",
+    description: "The chestnuts are the deciduous trees and shrubs in the genus Castanea, in the beech family Fagaceae. The name also refers to the edible nuts they produce.",
     price: 199.99,
-    category: "electronics",
-    image: "https://images.unsplash.com/photo-1484704849700-f032a568e944?w=500&q=80"
+    category: "flowering",
+    image: "src/images/chestnuttree_olenka-v-YHFKWiPMyl4-unsplash.jpg"
   },
   {
     id: 2,
-    name: "Smart Watch",
-    description: "Feature-rich smartwatch with health tracking",
+    name: "Magnolia",
+    description: "Magnolia is a large genus of about 210 to 340 flowering plant species in the subfamily Magnolioideae of the family Magnoliaceae.",
     price: 299.99,
-    category: "electronics",
-    image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500&q=80"
+    category: "flowering",
+    image: "src/images/magnoliatree_marina-yalanska-cc6jH1ILxkA-unsplash.jpg"
   },
   {
     id: 3,
-    name: "Leather Wallet",
-    description: "Handcrafted genuine leather wallet",
+    name: "Cherry",
+    description: "A cherry is the fruit of many plants of the genus Prunus, and is a fleshy drupe (stone fruit). Red cherries with stems Prunus avium, sweet cherry.",
     price: 49.99,
-    category: "accessories",
-    image: "https://images.unsplash.com/photo-1627123424574-724758594e93?w=500&q=80"
+    category: "flowering",
+    image: "src/images/cherrytree_kaley-obsidia-N1wW23Diszw-unsplash.jpg"
   },
   {
     id: 4,
-    name: "Sunglasses",
-    description: "Classic design sunglasses with UV protection",
+    name: "Oak",
+    description: "An oak is a hardwood tree or shrub in the genus Quercus of the beech family. They have spirally arranged leaves, often with lobed edges, and a nut called an acorn, borne within a cup.",
     price: 129.99,
-    category: "accessories",
-    image: "https://images.unsplash.com/photo-1572635196237-14b3f281503f?w=500&q=80"
+    category: "nonflowering",
+    image: "src/images/oaktree_kadir-celep-ptUu330lxrE-unsplash.jpg"
   }
 ];
